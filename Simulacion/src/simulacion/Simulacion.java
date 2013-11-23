c9dac182aac11145e5c91635a36c637e671c2bc5
lalokuyo <lalokuyo@gmail.com>
2013-11-23 14:16:34 -0600

    PRueba

--- Simulacion/src/simulacion/Simulacion.java
+++ Simulacion/src/simulacion/Simulacion.java
@@ -15,5 +15,8 @@ public class Simulacion {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("hola");
+        
     }
 }
