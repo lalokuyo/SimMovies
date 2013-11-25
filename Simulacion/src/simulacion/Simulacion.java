33215ee412330af53ec9042e54f8097f7a9d6b36
lalokuyo <lalokuyo@gmail.com>
2013-11-25 13:18:52 -0600

    Revert e19c65d..1e59f16
    This rolls back to commit e19c65d2d8d4ac8635045105376a47fa83c41441.

--- Simulacion/src/simulacion/Simulacion.java
+++ Simulacion/src/simulacion/Simulacion.java
@@ -14,21 +14,13 @@ public class Simulacion {
 
     /**
      * @param args the command line arguments
-     * 
      */
-     
-    
     public static void main(String[] args) {
-        
- // ************************************* VARIABLES **********************************************
-         //Lista de objetos 
+        // TODO code application logic here
+      
+        //Lista de objetos 
          List<Cliente> listaCliente = new ArrayList<Cliente>();
          List<Object> buffer = new ArrayList<Object>();
-         
-         //Ciclo de Clientes
-         int time = 0;
-         int numCliente= 0;
-         int size = listaCliente.size()-1;
          
          //Cliente nuevo
          Cliente cliente = new Cliente(2, 3);
@@ -40,7 +32,10 @@ public class Simulacion {
          listaCliente.add(cliente2);
          listaCliente.add(cliente3);
          
-         
+         int time = 0;
+         int numCliente= 0;
+         int i = 0;
+         int size = listaCliente.size()-1;
          
          //Ciclo de los clientes y del número de iteraciones
          while (time <= 100){
