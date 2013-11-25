1e59f16f2dfa0b304c9117f71c55c5c8daf87e67
lalokuyo <lalokuyo@gmail.com>
2013-11-25 13:18:43 -0600

    no

--- Simulacion/src/simulacion/Simulacion.java
+++ Simulacion/src/simulacion/Simulacion.java
@@ -14,13 +14,21 @@ public class Simulacion {
 
     /**
      * @param args the command line arguments
+     * 
      */
+     
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-      
-        //Lista de objetos 
+        
+ // ************************************* VARIABLES **********************************************
+         //Lista de objetos 
          List<Cliente> listaCliente = new ArrayList<Cliente>();
          List<Object> buffer = new ArrayList<Object>();
+         
+         //Ciclo de Clientes
+         int time = 0;
+         int numCliente= 0;
+         int size = listaCliente.size()-1;
          
          //Cliente nuevo
          Cliente cliente = new Cliente(2, 3);
@@ -32,10 +40,7 @@ public class Simulacion {
          listaCliente.add(cliente2);
          listaCliente.add(cliente3);
          
-         int time = 0;
-         int numCliente= 0;
-         int i = 0;
-         int size = listaCliente.size()-1;
+         
          
          //Ciclo de los clientes y del número de iteraciones
          while (time <= 100){
